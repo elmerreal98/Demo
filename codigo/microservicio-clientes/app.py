77f1f07515855e6a8622d5c17045e6bde97be5f8
elmerreal98 <elmerreal98@gmail.com>
2021-01-22 12:16:11 -0600

    Cambios cluester

--- codigo/microservicio-clientes/app.py
+++ codigo/microservicio-clientes/app.py
@@ -2,10 +2,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/crear')
 def hello():
-    return render_template('index.html')
+    return render_template('crear.html')
 
+@app.route('/listar')
+def hello():
+    return render_template('listar.html')
 
 
 if __name__ == '__main__':
